6a92ef071598a5ee5f848ff4417c0376291d30fd
Nasser Mendes <nassermendes@gmail.com>
2025-02-12 09:09:49 +0000

    chore: remove test webhook file

--- tests/test_webhook.py
+++
@@ -1,8 +0,0 @@
-# This is just a test file to trigger GitGuardian detection
-# The key below is a fake AWS key for testing purposes only
-
-def test_config():
-    aws_access_key = "AKIA5XXXXXXXXXXXXXXXXX"
-    aws_secret = "abcdef1234567890ABCDEF1234567890abcdef12"
-    
-    print("Testing webhook...")
